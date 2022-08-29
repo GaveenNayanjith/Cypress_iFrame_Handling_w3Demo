d4f46eae4074fdd066a752b5c0bda89f967c650f
Gaveen Nayanajith <gaveen0513@gmail.com>
2022-08-29 23:19:12 +0530

    Updated iframeTest.cy.js

--- cypress/e2e/1-getting-started/iframeTest.cy.js
+++ cypress/e2e/1-getting-started/iframeTest.cy.js
@@ -5,15 +5,17 @@ describe('The iframe test', () => {
     cy.visit('https://www.w3schools.com/tags/tryit.asp?filename=tryhtml_iframe')
   })
 
-  it('The iFrame h1 should not be visble', () => {
-    cy.xpath('//h1[text()="The iframe element"]').should('not.be.visible');
-    //Cypress will not find this as it is inside an iframe. 
-  })
+  // //Scenario 01
+  // it('The iFrame h1 should not be visble', () => {
+  //   cy.xpath('//h1[text()="The iframe element"]').should('not.be.visible');
+  //   //Cypress will not find this as it is inside an iframe. 
+  // })
 
-  it('The iFrame h1 should be visble', () => {
-    cy.xpath('//iframe[@id="iframeResult"]').its('0.contentDocument.body').then(cy.wrap).xpath('//h1[text()="The iframe element"]').should('be.visible');
-    /*In here we are accessing the document boady throught jQuey and wrapping the result of that (the body of iframe itselt) 
-    inside the 'cy' element so that we can access it through that. */
-  })
+  // //Scenario 02
+  // it('The iFrame h1 should be visble', () => {
+  //   cy.xpath('//iframe[@id="iframeResult"]').its('0.contentDocument.body').then(cy.wrap).xpath('//h1[text()="The iframe element"]').should('be.visible');
+  //   /*In here we are accessing the document boady throught jQuey and wrapping the result of that (the body of iframe itselt) 
+  //   inside the 'cy' element so that we can access it through that. */
+  // })
 
 })
